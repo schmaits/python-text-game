9df1fed848e7e86d6ea52822fdccc8695d6f549d
schmaits <stansfield.c.r@gmail.com>
2019-10-25 14:26:22 +0100

    Defines title screen selections function and title screen function

--- game.py
+++ game.py
@@ -18,3 +18,31 @@ class player:
 		self.status_effects = []
 myPlayer = player()
 
+##### Title Screen #####
+def title_screen_selections():
+	option = input('> ')
+	if option.lower() == ('play'):
+		state_game() # placeholder until written
+	elif option.lower() == ('help'):
+		help_menu() # placeholder until written
+	elif option.lower() == ('quit'):
+		sys.exit()
+	while option.lower() not in ['play', 'help', 'quit']:
+		print('I can't do that')
+		option = input('> ')
+		if option.lower() == ('play'):
+			state_game() # placeholder until written
+		elif option.lower() == ('help'):
+			help_menu() # placeholder until written
+		elif option.lower() == ('quit'):
+			sys.exit()
+
+def title_screen();
+	os.system('clear')
+	print('########################')
+	print('# Welcome to the game! #')
+	print('########################')
+	print('        - Play -        ')
+	print('        - Help -        ')
+	print('        - Quit -        ')
+	title_screen_selections()
